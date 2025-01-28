f65a47f7844494b0f91395668e614a20c7aff188
Alexander Harvey <alexjharvey@gmail.com>
2025-01-28 12:02:42 -0500

    Adding header component to astro project

---
+++ astro/src/data/company-details.ts
@@ -0,0 +1,12 @@
+import { EmitFlags } from "typescript";
+
+export const companyInfo = {
+    name: "Joe The Guy",
+    phone: "(843) 263-1737",
+    address: "123 Main St, Spartanburg, SC",
+    hours: {
+      weekdays: "7:30 AM - 6:00 PM",
+      weekends: "10:00 AM - 4:00 PM",
+    },
+    email: "info@JoeTheGuy.com",
+  };
--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily:{
+				raleway	: ['Raleway', 'sans-serif'],
+			},
+			colors: {
+			'bg-dark': '#1A1A1A',
+			'bg-light': '#FFFFFF',
+			'primary': '#F7A71D',
+			'primary-light': '#FFF27C',
+			'primary-dark': '#7F350B',
+			'secondary-dark': '#808080',
+			'secondary': '#B3B3B3',
+			'secondary-light': '#F5F5F5',
+			'accent1': '#02A5FF',
+			'accent-light': '#93D8FF',
+			},
+		},
+		darkMode: 'class',
 	},
 	plugins: [],
-}
+};
