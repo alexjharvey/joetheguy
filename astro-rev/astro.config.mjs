6c83d5e6be24d6717856247034939a480f380334
Alexander Harvey <alexjharvey@gmail.com>
2025-11-30 13:54:09 -0500

    Removal of buggy before and after slider component that was unused. Minor fix to astro config file where Images may have erroneously accepted video formats.

--- astro-rev/astro.config.mjs
+++ astro-rev/astro.config.mjs
@@ -15,8 +15,7 @@ export default defineConfig({
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
-    },
-    formats: ['webp', 'avif', 'png', 'jpg']
+    }
   },
   integrations: [tailwind(), react(), icon()],
   vite: {
